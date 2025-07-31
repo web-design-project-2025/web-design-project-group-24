cfb89fd471d8071c39adf1472d9eff04634774a4
moakajsa <grka24fe@student.ju.se>
2025-07-31 14:32:01 +0200

    js start

--- js/main.js
+++ js/main.js
@@ -1,2 +1,38 @@
 let events = [];
 let detailed = [];
+const contentElement = document.getElementById("content");
+
+async function loadData() {
+  const eventResponse = await fetch("data/events.json");
+  const eventJSON = await eventResponse.json();
+  events = eventJSON.events;
+
+  renderContent();
+}
+
+function getEventById(id) {
+  return events.find((event) => event.id === id);
+}
+
+function createEventContainer(event) {
+  const eventContainerElement = document.createElement("article");
+  eventContainerElement.classList.add("event-container");
+
+  const eventImg = document.createElement("img");
+  eventImg.classList.add("event-img");
+  eventImg.src = event.event_image;
+
+  return eventContainerElement;
+}
+
+function renderContent() {
+  contentElement.innerHTML = "";
+
+  for (let event of events) {
+    const event = getEventById(event.id);
+    const eventContainerElement = createEventContainer(event);
+    contentElement.appendChild(eventContainerElement);
+  }
+}
+
+loadData();
