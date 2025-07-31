ee659eec3e242360bcb2fac3a991e1a269415e93
moakajsa <grka24fe@student.ju.se>
2025-07-31 15:44:34 +0200

    event image loading

--- js/main.js
+++ js/main.js
@@ -1,2 +1,41 @@
 let events = [];
 let detailed = [];
+const allEventsContainerElement = document.getElementById(
+  "all-events-container"
+);
+
+async function loadData() {
+  const eventResponse = await fetch("data/events.json");
+  const eventJSON = await eventResponse.json();
+  events = eventJSON.events;
+
+  renderContent();
+}
+
+function getEventById(id) {
+  return events.find((event) => event.id === id);
+}
+
+function createEventContainer(event) {
+  const eventContainerElement = document.createElement("article");
+  eventContainerElement.classList.add("event-container");
+
+  const eventImg = document.createElement("img");
+  eventImg.classList.add("event-img");
+  eventImg.src = event.event_image;
+  eventContainerElement.appendChild(eventImg);
+
+  return eventContainerElement;
+}
+
+function renderContent() {
+  allEventsContainerElement.innerHTML = "";
+
+  for (let event of events) {
+    const eventDetails = getEventById(event.id);
+    const eventContainerElement = createEventContainer(eventDetails);
+    allEventsContainerElement.appendChild(eventContainerElement);
+  }
+}
+
+loadData();
