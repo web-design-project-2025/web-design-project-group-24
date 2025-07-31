4e05968b7401db25127c3a1616c21401c45fb1c0
moakajsa <grka24fe@student.ju.se>
2025-07-31 17:44:38 +0200

    adding more data

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let detailed = [];
 const allEventsContainerElement = document.getElementById(
   "all-events-container"
 );
+// const filterInput = document.getElementById("filter-input");
+// const filterResult = document.getElementById("filter-result");
 
 async function loadData() {
   const eventResponse = await fetch("data/events.json");
@@ -66,5 +68,33 @@ function renderContent() {
     allEventsContainerElement.appendChild(eventContainerElement);
   }
 }
+
+// Function to filter and display the results
+// filterInput.addEventListener("input", function () {
+//   const query = filterInput.value.toLowerCase();
+
+//   const filteredData = eventData.filter((event) => {
+//     return (
+//       event.event_name.toLowerCase().includes(query) ||
+//       event.event_tags.some((tag) => tag.toLowerCase().includes(query))
+//     );
+//   });
+
+//   // Display filtered results
+//   filteredResults.innerHTML = filteredData
+//     .map(
+//       (event) => `
+//         <div>
+//             <h3>${event.event_name}</h3>
+//             <p>${event.date_time_place}</p>
+//             <img src="${event.event_image}" alt="${
+//         event.event_name
+//       }" style="width:100px;height:auto;">
+//             <p>Tags: ${event.event_tags.join(", ")}</p>
+//         </div>
+//     `
+//     )
+//     .join("");
+// });
 
 loadData();
